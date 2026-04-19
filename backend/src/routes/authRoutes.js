b0990f78bd8ce0458a04b9fa20c3b7e2f0b1db7a
mahyhaker <136195321+Mahyhaker@users.noreply.github.com>
2026-04-19 11:08:07 -0300

    Add logout route to authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { login, callback, me } = require('../controllers/authController');
+const {
+  login,
+  callback,
+  me,
+  logout
+} = require('../controllers/authController');
 
 router.get('/login', login);
 router.get('/callback', callback);
 router.get('/me', me);
+router.post('/logout', logout);
 
 module.exports = router;
